e10533b41541378e19c4ca29e31b2b83181e2483
BasilOrien <cuentaparaestudiosf@gmail.com>
2023-02-11 13:38:10 -0300

    pjreset

--- src/database/database.js
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from "sequelize";
-
-export const sequelize = new Sequelize("bingo2", "postgres", "Aezakmi11", {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createPlayer, getPlayers } from "../controllers/index.controller.js";
-
-const router = Router();
-
-router.get("/", getPlayers);
-router.post("/", createPlayer);
-
-export default router;
